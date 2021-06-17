dbaabe4fd0246207bcf2d04a1ffae1637f131973
gzonelee <gzonelee@gmail.com>
2021-06-18 03:30:31 +0900

    Initial Commit

---
+++ WmpSU01/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WmpSU01
+//
+//  Created by gzonelee on 2021/06/18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WmpSU01/WmpSU01App.swift
@@ -0,0 +1,17 @@
+//
+//  WmpSU01App.swift
+//  WmpSU01
+//
+//  Created by gzonelee on 2021/06/18.
+//
+
+import SwiftUI
+
+@main
+struct WmpSU01App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
